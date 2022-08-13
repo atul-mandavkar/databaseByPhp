15b3391851c5243e41f585b072eafe174922af66
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2022-08-13 19:34:40 +0530

    Created database
    Created and selected database
    Created database query with new query
    and select database with new query

--- validateAndDb.php
+++ validateAndDb.php
@@ -4,7 +4,23 @@
     if(!$sRoll){
         die("Please enter a valid roll number of a student\n");
     }
-    if(!$sName){
+    else if(!$sName){
         die("Please enter a name of student<br>");
     }
-        ?>
+    else{
+        $con=mysqli_connect("localhost", "root", "");
+        if(!$con){
+            die("Not connnected to database server<br>");
+        }
+        else{
+            $qu1=mysqli_query($con, "CREATE DATABASE IF NOT EXISTS studInfo");
+            if(!$qu1){
+                die("Database studInfo is not selected<br>");
+            }
+        else    {
+            mysqli_query($con, "USE studInfo");
+            echo "Database studInfo is selected<br>";
+        }
+        }
+    }
+?>
